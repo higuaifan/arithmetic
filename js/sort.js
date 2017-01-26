fa1392dec5e363ca6f4a7e2e4586d05aeb6fe399
Higuaifan <zousitanghulu@foxmail.com>
2017-01-27 00:38:03 +0800

    add simple selection sort

--- js/sort.js
+++ js/sort.js
@@ -1,10 +1,10 @@
 // todo 模块化
 
 
-var testArray = [ 5,4,3,2,1,49, 38, 65, 97, 76, 13, 27, 49, 13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10];
+var testArray = [5, 4, 3, 2, 1, 49, 38, 65, 97, 76, 13, 27, 49, 13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10];
 
 //todo 这个写法好像有问题
-console.log(testArray);
+// console.log(testArray);
 //todo 用step抽象步长
 var straightInsert = function (array) {
     var sentry;
@@ -20,8 +20,9 @@ var straightInsert = function (array) {
     }
 };
 //。。瞎写的一个算步长的函数
+//todo 封装这两个函数
 var makeStap = function (length, stepArray) {
-    var array=stepArray;
+    var array = stepArray;
     var sqrt = Math.sqrt(45 + 36 * length);
     var x = (9 - sqrt) / 18 < 0 ? (9 + sqrt) / 18 : (9 - sqrt) / 18;
     var i = parseInt(Math.log(x) / Math.log(2));
@@ -41,30 +42,28 @@ var makeStap = function (length, stepArray) {
     }
     array.push(step);
 
-    array=makeStap(step - 1,array);
+    array = makeStap(step - 1, array);
     return array;
 };
 var shell = function (array) {
     var stepArray = [];//步长Array
     stepArray = makeStap(array.length, stepArray);
-    for(var i=0;i<stepArray.length;i++){
+    for (var i = 0; i < stepArray.length; i++) {
         //步长部分
-        var step=stepArray[i];
+        var step = stepArray[i];
 
-        for(var j=0;j<step;j++){
+        for (var j = 0; j < step; j++) {
 
-            for(var sentry=j;sentry<testArray.length;sentry+=step){
-                for(var m=j;m<sentry;m+=step){
+            for (var sentry = j; sentry < testArray.length; sentry += step) {
+                for (var m = j; m < sentry; m += step) {
 
-                    if(testArray[sentry]<testArray[m]){
+                    if (testArray[sentry] < testArray[m]) {
                         //如果有小于 往后移 m是移动的那个 
-                        var num=testArray[sentry];//那个数字提出，然后剩下的都要后移
-                        for(var l=sentry;l>m;l-=step){
-                            var a=testArray[l];
-                            var b=testArray[l-step];
-                            testArray[l]=testArray[l-step];
+                        var num = testArray[sentry];//那个数字提出，然后剩下的都要后移
+                        for (var l = sentry; l > m; l -= step) {
+                            testArray[l] = testArray[l - step];
                         }
-                        testArray[m]=num;
+                        testArray[m] = num;
                     }
                 }
             }
@@ -74,11 +73,32 @@ var shell = function (array) {
 };
 
 
+var simpleSelection = function (array) {
+    for(var i = 0;i<array.length;i++){
+        var minIndex=i;
+        for(var j = i;j<array.length;j++){
+            if(array[minIndex]>array[j]){
+                minIndex=j;
+            }
+        }
+        if(minIndex!=i){
+            array[i]=array[i]^array[minIndex];
+            array[minIndex]=array[i]^array[minIndex];
+            array[i]=array[i]^array[minIndex];
+        }
+
+    }
+};
+
 
 straightInsert(testArray);
 console.log(testArray);
 
 
-testArray = [ 5,4,3,2,1,49, 38, 65, 97, 76, 13, 27, 49, 13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10];
+testArray = [5, 4, 3, 2, 1, 49, 38, 65, 97, 76, 13, 27, 49, 13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10];
 shell(testArray);
+console.log(testArray);
+
+testArray = [5, 4, 3, 2, 1, 49, 38, 65, 97, 76, 13, 27, 49, 13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10];
+simpleSelection(testArray);
 console.log(testArray);
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 ## 排序
 1. 直插 Straight Insertion Sort
 2. 希尔 Shell's Sort
+3. 简单选择排序 Simple Selection Sort
